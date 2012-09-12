5b1c12bc9de280d0cd5dee8efe127129e4116c55
Alec Gorge <alecgorge@gmail.com>
2012-09-12 16:33:24 -0400

    ...

--- edline-api/edline-class.rb
+++ edline-api/edline-class.rb
@@ -55,7 +55,7 @@ class EdlineClass
 		# i = 0
 		# while c == nil and i < 5
 		# 	begin
-		puts "url: " << url
+		puts "url: " << url.to_s
 				c = @client.get(url,
 					:header => {'Referer' => 'https://www.edline.net/pages/Brebeuf'})
 		# 		break
